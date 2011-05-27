db9ba2684ce8cc8faf348f6746abefd8de1f56e1
Rahim Sonawalla <rsonawalla@gmail.com>
2011-05-27 09:50:14 -0700

    Clarify scoring prompt, increase timeout, add retry
    Clarify the scoring prompt so that the user knows that pressing 1
    accepts the lead and pressing any other key rejects the lead.
    
    Increase the timeout for the scoring so that the user has more time to
    make a decision.
    
    Add retries so that the user can still score the lead even if he/she
    missed the prompt the first time.

--- gather.php
+++ gather.php
@@ -2,7 +2,9 @@
 	header('content-type: text/xml');
 ?>
 <Response>
-<Gather action="handle-gather.php?agent=<?php echo $_REQUEST['agent']; ?>&amp;lead=<?php echo $_REQUEST['lead']; ?>" numDigits="1">
-		<Say voice="woman">Is this lead good?</Say>
+	<Gather action="handle-gather.php?agent=<?php echo $_REQUEST['agent']; ?>&amp;lead=<?php echo $_REQUEST['lead']; ?>" numDigits="1" timeout="15">
+		<Say voice="woman">Is this a good lead? Press 1 to accept the lead. Press any other key to reject the lead.</Say>
 	</Gather>
+	<Say voice="woman">I didn't receive any input.</Say>
+	<Redirect method="GET">gather.php?agent=<?php echo $_REQUEST['agent']; ?>&amp;lead=<?php echo $_REQUEST['lead']; ?></Redirect>
 </Response>
